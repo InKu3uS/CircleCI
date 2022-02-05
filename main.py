65fd124ef4345e9bf8cd845aa7a447a75d8e4712
InKu3uS <Neftarguez@gmail.com>
2022-02-05 18:09:07 +0000

    cambios

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def Add(a, b):
+def Suma(a, b):
         return a + b
         
 def DiHola():
         print("Hola a todos")
-        print ("5 + 5 es igual a", Add(5,5))
+        print ("5 + 5 es igual a", Suma(5,5))
 
 if __name__ == '__main__':
         DiHola()
