60cc69b2aa1d0dc4e92b5433a4d7514fe7f3eb7d
InKu3uS <Neftarguez@gmail.com>
2022-02-05 17:32:41 +0000

    Cambios

---
+++ main-test.py
@@ -0,0 +1,8 @@
+from main import Add
+
+def TestAdd():
+        assert Add(2,3) == 5
+        print("Esto funciona del 10")
+
+if __name__ == '__main__':
+        TestAdd()
---
+++ main.py
@@ -0,0 +1,8 @@
+def Add(a, b):
+        return a + b
+        
+def SayHello():
+        print("Hola a todos")
+
+if __name__ == '__main__':
+        SayHello()
