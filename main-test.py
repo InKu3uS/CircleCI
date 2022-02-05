791e110dc44c751cb1191d528c51dec22b91aa1c
InKu3uS <Neftarguez@gmail.com>
2022-02-05 18:01:21 +0000

    cambios

--- main-test.py
+++ main-test.py
@@ -4,7 +4,6 @@ def TestAdd():
         
         assert Add(2,3) == 5
         print("Esto funciona del 10")
-        
 
 if __name__ == '__main__':
         TestAdd()
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def Add(a, b):
         
 def DiHola():
         print("Hola a todos")
+        print ("5 + 5 es igual a", Add(5,5))
 
 if __name__ == '__main__':
         DiHola()
